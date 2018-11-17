f3da4ac4a6dc3ff5aa1f3f3c40911920d8187789
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-17 21:23:46 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -1,9 +1,11 @@
-def using_times
-	#your code here
-end
+
 jewels_in_bag = 100
 
 3.times do
   puts "Hiding 10 stolen jewels."
   jewels_in_bag = jewels_in_bag - 10
+  puts "#{{jewels_in_bag}}"
+end
+def using_times
+	#your code here
 end
